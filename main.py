2ca7ac6f17aa8cc0ca661819879f9b91f3e00168
Thomas Jeffery <thomas.jeffery.aws@gmail.com>
2026-01-17 17:57:21 +0100

    Sending local filepath of the latest picture instead of copying from the database (better performance)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import requests, random
-import uuid, os
+import uuid, os, shutil
 import sqlite3
 
 
@@ -47,31 +47,18 @@ def download_picture(item_id: str, q: int | None = None):
         with open(f"pictures/{filename}", "wb") as f:
             f.write(response.content)
         cursor.execute("INSERT INTO files (name, data) VALUES (?, ?)", (filename, response.content))
+        shutil.move("pictures/" + filename, "pictures/latest_picture.jpg")
     conn.commit()
     conn.close()
+    
 
-    return {"message": "Picture(s)s downloaded"}
+    return {"message": "Picture(s) downloaded"}
 
 
 @app.get("/picture/latest")
 def get_latest_picture():
-    conn = sqlite3.connect('picture_database.db')
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT name, data FROM files ORDER BY id DESC LIMIT 1")
-    row = cursor.fetchone()
-    if row:
-        name, data = row
-        if not os.path.exists("pictures"):
-            os.makedirs("pictures")
-        filepath = os.path.join("pictures", "latest_picture.jpg")
-        os.makedirs(os.path.dirname(filepath), exist_ok=True)
-        with open(filepath, "wb") as f:
-            f.write(data)
-        conn.commit()
-        conn.close()
-        return {"message": f"Latest picture '{name}' downloaded"}
+    if os.path.exists("pictures/latest_picture.jpg"):
+        return ({"message": "pictures/latest_picture.jpg"})
     else:
-        conn.commit()
-        conn.close()
-        return {"message": "No pictures found in the database"}
+        return ({"message": "No picture found"})
+        
