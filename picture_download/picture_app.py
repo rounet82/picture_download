4d26db0c5f9b2e5aefa6cabf7bff17b9c67a5915
Thomas Jeffery <thomas.jeffery.aws@gmail.com>
2026-01-18 14:13:48 +0100

    Adding the streamlit app to the container file.

--- picture_download/picture_app.py
+++ picture_download/picture_app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import requests
 from PIL import Image
+import os
 
 def fetch_data(url):
     response = requests.get(url)
     return response.json()
 
+# Use API service name when in Docker, localhost otherwise
+API_HOST = os.getenv("API_HOST", "127.0.0.1")
+API_PORT = os.getenv("API_PORT", "8000")
+API_URL = f"http://{API_HOST}:{API_PORT}"
 
 st.title("Select the animal and the number of pictures to download")
 
@@ -16,12 +21,12 @@ numbers = list(range(1, 11))  # [1, 2, ..., 10]
 num = st.selectbox("Choose a number:", numbers, index=0)
 
 if st.button("Go"):
-    data = fetch_data(f"http://127.0.0.1:8000/picture/{animal}/{num}")
+    data = fetch_data(f"{API_URL}/picture/{animal}/{num}")
     st.write(data["message"])
 
 st.title("Get the latest downloaded picture")
 if st.button("Get Latest Picture"):
-    data = fetch_data("http://127.0.0.1:8000/picture/latest")
+    data = fetch_data(f"{API_URL}/picture/latest")
     print(data["message"])
     img = Image.open(data["message"])
     st.image(img, caption="Latest picture", width="stretch")
